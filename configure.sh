d5f9970c14ac993ffc265df6f0b504b8e33ab548
wsad5354 <50922795+wsad5354@users.noreply.github.com>
2020-04-28 04:07:42 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,6 +14,9 @@ rm -rf /tmp/v2ray
 install -d /usr/local/etc/v2ray
 cat << EOF > /usr/local/etc/v2ray/config.json
 {
+    "log": {
+        "loglevel": "warning"
+    },
     "inbound": {
         "protocol": "vmess",
         "listen": "127.0.0.1",
